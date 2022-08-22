3ee072c1ebcde227981247209b1ded1fd44a5147
your name <your email>
2022-08-22 12:05:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/login_actividad1/data/LoginDataSource.kt
@@ -0,0 +1,24 @@
+package com.example.login_actividad1.data
+
+import com.example.login_actividad1.data.model.LoggedInUser
+import java.io.IOException
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+class LoginDataSource {
+
+    fun login(username: String, password: String): Result<LoggedInUser> {
+        try {
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), " tu usuario es: $username y tu contraseña:${password}")
+            return Result.Success(fakeUser)
+        } catch (e: Throwable) {
+            return Result.Error(IOException("Error logging in", e))
+        }
+    }
+
+    fun logout() {
+
+
+    }
+}
